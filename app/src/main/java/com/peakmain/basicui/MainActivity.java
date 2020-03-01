dfa69023c70d283e962f409ddafdb86351945929
peakmain <2726449200@qq.com>
2020-03-01 16:18:07 +0800

    update md file

--- app/src/main/java/com/peakmain/basicui/MainActivity.java
+++ app/src/main/java/com/peakmain/basicui/MainActivity.java
@@ -1,8 +1,8 @@
 package com.peakmain.basicui;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
