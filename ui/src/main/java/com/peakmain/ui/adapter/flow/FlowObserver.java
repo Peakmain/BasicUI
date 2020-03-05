461dd2800371e24586ecb24bdd39b282ac5a515f
peakmain <2726449200@qq.com>
2020-03-05 19:07:16 +0800

    流式布局的修改

---
+++ ui/src/main/java/com/peakmain/ui/adapter/flow/FlowObserver.java
@@ -0,0 +1,11 @@
+package com.peakmain.ui.adapter.flow;
+
+/**
+ * author ：Peakmain
+ * createTime：2020/3/5
+ * mail:2726449200@qq.com
+ * describe：流式布局的观察者
+ */
+public abstract class FlowObserver {
+    public abstract void notifyDataChange();
+}
