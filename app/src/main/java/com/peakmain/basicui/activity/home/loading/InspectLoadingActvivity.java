664fb9e32b234720a23f51955eeacfe5109317e1
peakmain <2726449200@qq.com>
2020-03-17 20:31:52 +0800

    视察

---
+++ app/src/main/java/com/peakmain/basicui/activity/home/loading/InspectLoadingActvivity.java
@@ -0,0 +1,39 @@
+package com.peakmain.basicui.activity.home.loading;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import com.peakmain.basicui.R;
+import com.peakmain.basicui.base.BaseActivity;
+import com.peakmain.ui.loading.InspectLoadingView;
+
+/**
+ * author ：Peakmain
+ * createTime：2020/3/17
+ * mail:2726449200@qq.com
+ * describe：
+ */
+public class InspectLoadingActvivity extends BaseActivity {
+    private InspectLoadingView mInspectLoadingView;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_inspect_loading;
+    }
+
+    @Override
+    protected void initView() {
+        mInspectLoadingView = findViewById(R.id.inspect_loading_view);
+        mNavigationBuilder.setTitleText("视察动画的loading").create();
+    }
+
+    @Override
+    protected void initData() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mInspectLoadingView.disappear();
+            }
+        },2000);
+    }
+}
