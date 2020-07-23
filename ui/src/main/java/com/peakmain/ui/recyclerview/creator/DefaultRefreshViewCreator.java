e91f0f0a00803b5c44d1b14a2e51b271bf2f540a
peakmain <2726449200@qq.com>
2020-07-23 09:57:46 +0800

    下拉刷新时间

--- ui/src/main/java/com/peakmain/ui/recyclerview/creator/DefaultRefreshViewCreator.java
+++ ui/src/main/java/com/peakmain/ui/recyclerview/creator/DefaultRefreshViewCreator.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.peakmain.ui.R;
 import com.peakmain.ui.recyclerview.view.LoadRefreshRecyclerView;
+import com.peakmain.ui.utils.PreferencesUtil;
 
 /**
  * author ：Peakmain
@@ -22,7 +23,7 @@ public class DefaultRefreshViewCreator extends RefreshViewCreator {
     private TextView mTvStatus;
     private TextView mTvRefreshTime;
     private ImageView mIvArrow;
-
+    private Context mContext;
 
     /**
      * @param context 上下文
@@ -35,6 +36,8 @@ public class DefaultRefreshViewCreator extends RefreshViewCreator {
         mTvStatus = refreshView.findViewById(R.id.tv_refresh_status);
         mTvRefreshTime = refreshView.findViewById(R.id.tv_refresh_time);
         mIvArrow = refreshView.findViewById(R.id.iv_arrow_downward);
+
+        this.mContext = context;
         return refreshView;
     }
 
@@ -47,7 +50,8 @@ public class DefaultRefreshViewCreator extends RefreshViewCreator {
      */
     @Override
     public void onPull(int currentDragHeight, int refreshViewHeight, int currentRefreshStatus) {
-        mTvRefreshTime.setText("刷新时间:2020年");
+        /*mTvRefreshTime.setText("刷新时间:2020年");*/
+        mTvRefreshTime.setText(PreferencesUtil.getInstance(mContext).getRefreshTime());
         if (currentRefreshStatus == LoadRefreshRecyclerView.LOAD_STATUS_PULL_DOWN_REFRESH) {
             mTvStatus.setText("下拉可以刷新");
             mIvArrow.setImageResource(R.drawable.ic_arrow_downward);
@@ -63,15 +67,16 @@ public class DefaultRefreshViewCreator extends RefreshViewCreator {
     public void onRefreshing() {
         mTvStatus.setText("正在刷新....");
         mIvArrow.setImageResource(R.drawable.ic_progress_circle);
-        RotateAnimation animation=new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,
-                Animation.RELATIVE_TO_SELF,0.5f);
+        RotateAnimation animation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
         animation.setRepeatCount(-1);
         animation.setDuration(1000);
         mIvArrow.startAnimation(animation);
+        PreferencesUtil.getInstance(mContext).saveRefreshTime();
     }
 
     @Override
     public void onStopRefresh() {
-       mIvArrow.clearAnimation();
+        mIvArrow.clearAnimation();
     }
 }
