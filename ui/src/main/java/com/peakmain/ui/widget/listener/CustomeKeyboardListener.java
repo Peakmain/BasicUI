96a52318927e3a12a7caff83d819937ff51f7ac9
peakmain <2726449200@qq.com>
2020-12-23 19:43:57 +0800

    键盘

---
+++ ui/src/main/java/com/peakmain/ui/widget/listener/CustomeKeyboardListener.java
@@ -0,0 +1,15 @@
+package com.peakmain.ui.widget.listener;
+
+/**
+ * author ：Peakmain
+ * createTime：2020/12/23
+ * mail:2726449200@qq.com
+ * describe：自定义键盘监听事件
+ */
+public interface CustomeKeyboardListener {
+    public void click(String number);
+
+    public void delete();
+
+    public void dissmiss();
+}
