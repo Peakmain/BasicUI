65e9ab786c48aa3471970607575c327fc721a0ed
peakmain <2726449200@qq.com>
2021-04-18 00:33:38 +0800

    文件阅读器

---
+++ ui/src/main/java/com/peakmain/ui/read/X5WebViewManager.kt
@@ -0,0 +1,48 @@
+package com.xuantian.common.ui.read
+
+import android.content.Context
+import android.util.Log
+import com.tencent.smtt.sdk.QbSdk
+import com.tencent.smtt.sdk.QbSdk.PreInitCallback
+
+/**
+ * author ：Peakmain
+ * createTime：2021/4/16
+ * mail:2726449200@qq.com
+ * describe：
+ */
+internal class X5WebViewManager private constructor() {
+
+    /**
+     * 初始化x5内核
+     *
+     * @param context
+     */
+    fun initX5Web(context: Context) {
+        QbSdk.setDownloadWithoutWifi(true)
+        QbSdk.initX5Environment(context.applicationContext, object : PreInitCallback {
+            override fun onViewInitFinished(arg0: Boolean) {
+                Log.d("X5WebViewManager", "X5内核初始化:$arg0")
+            }
+
+            override fun onCoreInitFinished() {
+                Log.d("X5WebViewManager", "X5内核开始初始化完成")
+            }
+        })
+    }
+
+    companion object {
+        var instance: X5WebViewManager? = null
+            get() {
+                if (field == null) {
+                    synchronized(X5WebViewManager::class.java) {
+                        if (field === field) {
+                            field = X5WebViewManager()
+                        }
+                    }
+                }
+                return field
+            }
+            private set
+    }
+}
