1602ed930d19334e657e5f20bbd08c469581b59f
2726449200@qq.com <3nhwng9xzw@privaterelay.appleid.com>
2021-04-30 11:44:50 +0800

    bitmap工具类

---
+++ ui/src/main/java/com/peakmain/ui/utils/BitmapUtils.kt
@@ -0,0 +1,59 @@
+package com.peakmain.ui.utils
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.view.View
+import android.widget.ImageView
+
+/**
+ * author ：Peakmain
+ * createTime：2021/4/30
+ * mail:2726449200@qq.com
+ * describe：Bitmap工具类
+ */
+object BitmapUtils {
+    private val mCanvas = Canvas()
+    fun Bitmap.createBitmapFromView(view: View): Bitmap? {
+        return createBitmapFromView(view, 1f)
+    }
+
+    fun Bitmap.createBitmapFromView(view: View, scale: Float): Bitmap? {
+        if (view is ImageView) {
+            val drawable = view.drawable
+            if (drawable != null && drawable is BitmapDrawable) {
+                return drawable.bitmap
+            }
+        }
+        view.clearFocus()
+        val bitmap = createBitmapSafely((view.width * scale.toInt()), view.height * scale.toInt(), Bitmap.Config.ARGB_8888, 1)
+        if (bitmap != null) {
+            synchronized(mCanvas) {
+                val canvas = mCanvas
+                canvas.setBitmap(bitmap)
+                canvas.save()
+                canvas.drawColor(Color.WHITE)
+                canvas.scale(scale, scale)
+                view.draw(canvas)
+                canvas.restore()
+                canvas.setBitmap(null)
+            }
+        }
+        return bitmap
+    }
+
+
+    fun Bitmap.createBitmapSafely(width: Int, height: Int, config: Bitmap.Config, retryCount: Int): Bitmap? {
+        try {
+            Bitmap.createBitmap(width, height, config)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (retryCount > 0) {
+                System.gc()
+                return createBitmapSafely(width, height, config, retryCount)
+            }
+        }
+        return null
+    }
+}
