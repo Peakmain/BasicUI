fd8c86b3806b09537c964bc004d952b076d02157
2726449200@qq.com <3nhwng9xzw@privaterelay.appleid.com>
2021-05-06 15:18:16 +0800

    监听fps的实现

---
+++ ui/src/main/java/com/peakmain/ui/utils/ActivityUtils.kt
@@ -0,0 +1,118 @@
+package com.peakmain.ui.utils
+
+import android.app.Activity
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import com.peakmain.ui.constants.BasicUIUtils
+import java.lang.ref.WeakReference
+import kotlin.math.acos
+
+/**
+ * author ：Peakmain
+ * createTime：2021/5/6
+ * mail:2726449200@qq.com
+ * describe：
+ */
+class ActivityUtils {
+
+    private val mActivityLists = ArrayList<WeakReference<Activity>>()
+
+    //前台后台回调
+    private val mFrontBackCallbacks = ArrayList<FrontBackCallback>()
+
+    //上一个阶段是否是前台
+    var isFront = true
+    private var mActivityStartCount = 0//>0表示应用在前台
+    fun init(application: Application) {
+        application.registerActivityLifecycleCallbacks(ActivityLifecycleCallbacks())
+    }
+
+    inner class ActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
+        override fun onActivityPaused(p0: Activity?) {
+
+        }
+
+        override fun onActivityResumed(p0: Activity?) {
+        }
+
+        override fun onActivityStarted(p0: Activity?) {
+            mActivityStartCount++
+            //!isFront表示之前处于后台
+            if (!isFront && mActivityStartCount > 0) {
+                isFront = true
+                onFrontBackChanged(isFront)
+            }
+        }
+
+        override fun onActivityDestroyed(p0: Activity?) {
+            for (activityRef in mActivityLists) {
+                if (activityRef.get() == p0) {
+                    mActivityLists.remove(activityRef);
+                    break
+                }
+            }
+
+        }
+
+        override fun onActivitySaveInstanceState(p0: Activity?, p1: Bundle?) {
+        }
+
+        override fun onActivityStopped(p0: Activity?) {
+            mActivityStartCount--
+            if (mActivityStartCount <= 0 && isFront) {
+                isFront = false
+                onFrontBackChanged(isFront)
+            }
+        }
+
+        override fun onActivityCreated(p0: Activity?, p1: Bundle?) {
+            mActivityLists.add(WeakReference(p0!!))
+        }
+
+    }
+
+    private fun onFrontBackChanged(front: Boolean) {
+        for (callback in mFrontBackCallbacks) {
+            callback.onChanged(front)
+        }
+    }
+
+    fun addFrontBackCallback(callback: FrontBackCallback) {
+        if (!mFrontBackCallbacks.contains(callback)) {
+            mFrontBackCallbacks.add(callback)
+        }
+    }
+
+    fun removeFrontBackCallback(callback: FrontBackCallback) {
+        mFrontBackCallbacks.remove(callback);
+    }
+
+    interface FrontBackCallback {
+        fun onChanged(front: Boolean)
+    }
+
+    companion object {
+        private val mApplication = BasicUIUtils.application!!
+
+        /**
+         * 跳转到悬浮窗设置界面
+         */
+        fun startOverlaySettingActivity() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mApplication.startActivity(Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + mApplication.packageName)
+                ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            }
+        }
+
+        @JvmStatic
+        val mInstance: ActivityUtils by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+            ActivityUtils()
+        }
+    }
+}
