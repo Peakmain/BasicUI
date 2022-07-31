46a856bc7078f44e9a14f22c8cfa2e34aaac2b88
peakmain <2726449200@qq.com>
2022-07-31 17:37:48 +0800

    demo设置文本

--- app/src/main/java/com/peakmain/basicui/activity/home/AutoEditTextActivity.kt
+++ app/src/main/java/com/peakmain/basicui/activity/home/AutoEditTextActivity.kt
@@ -1,7 +1,9 @@
 package com.peakmain.basicui.activity.home
 
+import android.widget.EditText
 import com.peakmain.basicui.R
 import com.peakmain.basicui.base.BaseActivity
+import com.peakmain.ui.widget.AutoDeleteEditText
 
 /**
  * author ：Peakmain
@@ -17,6 +19,7 @@ class AutoEditTextActivity : BaseActivity() {
     override fun initView() {
         mNavigationBuilder!!.setTitleText("AutoEditText的使用")
                 .create()
+         findViewById<AutoDeleteEditText>(R.id.auto_edit_text).setText("你好，我是Peakmain")
     }
 
     override fun initData() {}
