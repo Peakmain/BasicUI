0f2021a2412e469847d840900832fca3b6d10def
peakmain <chengdong.zhou@yaduo.com>
2022-09-28 16:20:22 +0800

    ShapeConstraintLayout

--- ui/src/main/java/com/peakmain/ui/widget/ShapeConstraintLayout.kt
+++ ui/src/main/java/com/peakmain/ui/widget/ShapeConstraintLayout.kt
@@ -3,16 +3,20 @@ package com.peakmain.ui.widget
 import android.content.Context
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.peakmain.ui.R
 
 /**
  * author ：Peakmain
- * createTime：2020/3/1
+ * createTime：2022/09/28
  * mail:2726449200@qq.com
- * describe：自定义LinearLayout
+ * describe：
  */
-class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
+class ShapeConstraintLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
     private var mRadius = 0f
 
     //线条的颜色、宽度
@@ -31,59 +35,59 @@ class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: Attri
     //渐变结束颜色
     private var mEndColor = 0
 
-    /**
-     * 初始化属性
-     */
     private fun init(context: Context, attrs: AttributeSet?) {
-        val ta = context.obtainStyledAttributes(attrs, R.styleable.ShapeLinearLayout)
-        mNormalBackgroundColor = ta.getColor(R.styleable.ShapeLinearLayout_shapeLlBackgroundColor, 0)
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.ShapeConstraintLayout)
+        mNormalBackgroundColor =
+            ta.getColor(R.styleable.ShapeConstraintLayout_shapeClBackgroundColor, 0)
         mGradientDrawable = GradientDrawable()
-        mRadius = ta.getDimensionPixelSize(R.styleable.ShapeLinearLayout_shapeLlRadius, 0).toFloat()
-        mNormalStrokeColor = ta.getColor(R.styleable.ShapeLinearLayout_shapeLlStrokeColor, 0)
-        mNormalStrokeWidth = ta.getDimensionPixelSize(R.styleable.ShapeLinearLayout_shapeLlStrokeWidth, 0)
-        mStartColor = ta.getColor(R.styleable.ShapeLinearLayout_shapeLlStartColor, 0)
-        mEndColor = ta.getColor(R.styleable.ShapeLinearLayout_shapeLlEndColor, 0)
+        mRadius =
+            ta.getDimensionPixelSize(R.styleable.ShapeConstraintLayout_shapeClRadius, 0).toFloat()
+        mNormalStrokeColor = ta.getColor(R.styleable.ShapeConstraintLayout_shapeClStrokeColor, 0)
+        mNormalStrokeWidth = ta.getColor(R.styleable.ShapeConstraintLayout_shapeClStrokeWidth, 0)
+        mStartColor = ta.getColor(R.styleable.ShapeConstraintLayout_shapeClStartColor, 0)
+        mEndColor = ta.getColor(R.styleable.ShapeConstraintLayout_shapeClEndColor, 0)
         val topLeftRadius: Int = ta.getDimensionPixelSize(
-                R.styleable.ShapeLinearLayout_shapeLlTopLeftRadius, mRadius.toInt())
+            R.styleable.ShapeConstraintLayout_shapeClTopLeftRadius, mRadius.toInt()
+        )
         val topRightRadius: Int = ta.getDimensionPixelSize(
-                R.styleable.ShapeLinearLayout_shapeLlTopRightRadius, mRadius.toInt())
+            R.styleable.ShapeConstraintLayout_shapeClTopRightRadius, mRadius.toInt()
+        )
         val bottomLeftRadius: Int = ta.getDimensionPixelSize(
-                R.styleable.ShapeLinearLayout_shapeLlBottomLeftRadius, mRadius.toInt())
+            R.styleable.ShapeConstraintLayout_shapeClBottomLeftRadius, mRadius.toInt()
+        )
         val bottomRightRadius: Int = ta.getDimensionPixelSize(
-                R.styleable.ShapeLinearLayout_shapeLlBottomRightRadius, mRadius.toInt())
+            R.styleable.ShapeConstraintLayout_shapeClBottomRightRadius, mRadius.toInt()
+        )
         if (topLeftRadius != mRadius.toInt() || topRightRadius != mRadius.toInt() || bottomLeftRadius != mRadius.toInt() || bottomRightRadius != mRadius.toInt()) {
-            setCornerRadii(floatArrayOf(
+            setCornerRadii(
+                floatArrayOf(
                     topLeftRadius.toFloat(), topLeftRadius.toFloat(),
                     topRightRadius.toFloat(), topRightRadius.toFloat(),
                     bottomRightRadius.toFloat(), bottomRightRadius.toFloat(),
                     bottomLeftRadius.toFloat(), bottomLeftRadius
-                    .toFloat()))
+                        .toFloat()
+                )
+            )
         }
         ta.recycle()
         background = mGradientDrawable
-
-
         setStroke()
     }
-
     /**
      * 设置边角背景及边线
      */
     private fun setStroke() {
-        //设置背景色
         mGradientDrawable.setColor(mNormalBackgroundColor)
-        //设置弧度
-        if (mRadius != 0f) {
-            mGradientDrawable.cornerRadius = mRadius
-        }
+        if (mRadius != 0f) mGradientDrawable.cornerRadius = mRadius
         //设置边线
-        mGradientDrawable.setStroke(mNormalStrokeWidth, mNormalBackgroundColor)
-        if (mStartColor != 0 && mEndColor != 0) {
-            mGradientDrawable.orientation = GradientDrawable.Orientation.LEFT_RIGHT
-            mGradientDrawable.colors = intArrayOf(mStartColor, mEndColor)
+        if (mNormalStrokeWidth != 0 && mNormalStrokeColor != 0)
+            mGradientDrawable.setStroke(mNormalStrokeWidth, mNormalStrokeColor)
+
+        if(mStartColor!=0&&mEndColor!=0){
+            mGradientDrawable.orientation=GradientDrawable.Orientation.LEFT_RIGHT
+            mGradientDrawable.colors= intArrayOf(mStartColor,mEndColor)
         }
     }
-
     /**
      * 设置圆弧
      * @param radius 圆弧的角度
@@ -93,7 +97,6 @@ class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: Attri
         mGradientDrawable.cornerRadius = mRadius
         setStroke()
     }
-
     /**
      * 设置线宽度
      * @param strokeWidth 线条的宽度
@@ -103,7 +106,6 @@ class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: Attri
         mGradientDrawable.setStroke(mNormalStrokeWidth, mNormalStrokeColor)
         setStroke()
     }
-
     /**
      * 设置线的颜色
      * @param strokeColor 线条的颜色
@@ -113,7 +115,6 @@ class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: Attri
         mGradientDrawable.setStroke(mNormalStrokeWidth, mNormalStrokeColor)
         setStroke()
     }
-
     /**
      * 设置背景色
      * @param backgroundColor 背景颜色
@@ -124,9 +125,6 @@ class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: Attri
         setStroke()
     }
 
-    /**
-     * 设置四周的圆角
-     */
     private fun setCornerRadii(radius: FloatArray) {
         mGradientDrawable.cornerRadii = radius
     }
@@ -134,4 +132,6 @@ class ShapeLinearLayout @JvmOverloads constructor(context: Context, attrs: Attri
     init {
         init(context, attrs)
     }
+
+
 }
