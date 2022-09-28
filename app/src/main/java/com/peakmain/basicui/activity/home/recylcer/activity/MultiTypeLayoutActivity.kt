5397d1b399e00260c6f43daace7a2be03a5d43b7
peakmain <chengdong.zhou@yaduo.com>
2022-09-28 16:35:44 +0800

    demo修改

--- app/src/main/java/com/peakmain/basicui/activity/home/recylcer/activity/MultiTypeLayoutActivity.kt
+++ app/src/main/java/com/peakmain/basicui/activity/home/recylcer/activity/MultiTypeLayoutActivity.kt
@@ -43,7 +43,7 @@ class MultiTypeLayoutActivity : BaseActivity() {
 
             for (i in 0..19) {
                 if ( i % 3 == 0) {
-                    list.add("https://gank.io/images/882afc997ad84f8ab2a313f6ce0f3522")
+                    list.add("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Flmg.jj20.com%2Fup%2Fallimg%2F1111%2F03041Q50536%2F1P304150536-7-1200.jpg&refer=http%3A%2F%2Flmg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1666945985&t=1541c1c1e1dbc052d4310809aedec13a")
                 }else{
                     list.add("数据:$i")
                 }
