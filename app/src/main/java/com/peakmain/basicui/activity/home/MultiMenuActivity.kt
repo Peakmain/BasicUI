9db0e2b3523d42663ae293788725afeb21bf9db3
peakmain <2726449200@qq.com>
2023-04-14 14:30:30 +0800

    酒店品牌demo

--- app/src/main/java/com/peakmain/basicui/activity/home/MultiMenuActivity.kt
+++ app/src/main/java/com/peakmain/basicui/activity/home/MultiMenuActivity.kt
@@ -5,6 +5,7 @@ import com.peakmain.basicui.adapter.ListMenuAdapter
 import com.peakmain.basicui.base.BaseActivity
 import com.peakmain.ui.widget.menu.ListMenuView
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * author ：Peakmain
@@ -27,11 +28,25 @@ class MultiMenuActivity : BaseActivity() {
 
     override fun initData() {
         mData = ArrayList()
-        mData.add("类型")
-        mData.add("品牌")
-        mData.add("价格")
-        mData.add("更多")
-        mAdapter = ListMenuAdapter(this, mData)
+        mData.add("推荐排序")
+        mData.add("酒店品牌")
+        mData.add("热门地点")
+        mData.add("综合筛选")
+
+        //初始化推荐排序
+        val recommendSortList = ArrayList<String>()
+        recommendSortList.add("推荐排序")
+        recommendSortList.add("距离优先")
+        recommendSortList.add("好评优先")
+
+        //品牌
+        val brandList = arrayOf(
+            "高品质人文酒店品牌", "精品主题酒店品牌",
+            "高品质非标酒店品牌", "高品质商旅酒店品牌", "豪华生活方式品牌", "专注青年商旅酒店品牌", "Z时代生活方式品牌"
+        ).toMutableList()
+
+
+        mAdapter = ListMenuAdapter(this, mData, recommendSortList,brandList)
         mMenuView!!.setAdapter(mAdapter)
     }
 }
