2115cb74cce6630529721b01576da30d6343c5cb
peakmain <2726449200@qq.com>
2023-04-19 17:39:18 +0800

    代码优化

--- app/src/test/java/com/peakmain/basicui/AppTest.kt
+++ app/src/test/java/com/peakmain/basicui/AppTest.kt
@@ -1,18 +1,9 @@
 package com.peakmain.basicui
 
-import com.peakmain.ui.BasicUIProvider
-import com.peakmain.ui.constants.BasicUIUtils
-import org.junit.Assert
-import org.junit.Test
-
 /**
  * author ：Peakmain
  * createTime：2023/04/19
  * mail:2726449200@qq.com
  * describe：
  */
-class AppTest {
-    @Test
-    fun testApplication() {
-    }
-}
+class AppTest
