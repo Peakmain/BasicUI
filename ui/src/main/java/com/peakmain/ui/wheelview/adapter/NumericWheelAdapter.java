d5b6711bcdd7608aa71d725ee88e2bf07e6407e8
peakmain <2726449200@qq.com>
2023-04-19 14:40:35 +0800

    代码和文档优化

--- ui/src/main/java/com/peakmain/ui/wheelview/adapter/NumericWheelAdapter.java
+++ ui/src/main/java/com/peakmain/ui/wheelview/adapter/NumericWheelAdapter.java
@@ -9,39 +9,40 @@ package com.peakmain.ui.wheelview.adapter;
  */
 public class NumericWheelAdapter implements WheelAdapter<Object> {
 
-	private final int minValue;
-	private final int maxValue;
+    private final int minValue;
+    private final int maxValue;
 
-	/**
-	 * Constructor
-	 * @param minValue the wheel min value
-	 * @param maxValue the wheel max value
-	 */
-	public NumericWheelAdapter(int minValue, int maxValue) {
-		this.minValue = minValue;
-		this.maxValue = maxValue;
-	}
+    /**
+     * Constructor
+     *
+     * @param minValue the wheel min value
+     * @param maxValue the wheel max value
+     */
+    public NumericWheelAdapter(int minValue, int maxValue) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
 
-	@Override
-	public Object getItem(int index) {
-		if (index >= 0 && index < getItemsCount()) {
-			return minValue + index;
-		}
-		return 0;
-	}
+    @Override
+    public Object getItem(int index) {
+        if (index >= 0 && index < getItemsCount()) {
+            return minValue + index;
+        }
+        return 0;
+    }
 
-	@Override
-	public int getItemsCount() {
-		return maxValue - minValue + 1;
-	}
+    @Override
+    public int getItemsCount() {
+        return maxValue - minValue + 1;
+    }
 
-	@Override
-	public int indexOf(Object o){
-		try {
-			return (int)o - minValue;
-		} catch (Exception e) {
-			return -1;
-		}
+    @Override
+    public int indexOf(Object o) {
+        try {
+            return (int) o - minValue;
+        } catch (Exception e) {
+            return -1;
+        }
 
-	}
+    }
 }
