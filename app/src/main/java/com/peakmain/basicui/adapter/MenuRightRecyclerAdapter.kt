fdf4b5371086fe6ad884e15e1bcbf67099b2a7f0
peakmain <2726449200@qq.com>
2023-05-11 10:09:37 +0800

    支持动态设置标题

--- app/src/main/java/com/peakmain/basicui/adapter/MenuRightRecyclerAdapter.kt
+++ app/src/main/java/com/peakmain/basicui/adapter/MenuRightRecyclerAdapter.kt
@@ -1,10 +1,11 @@
 package com.peakmain.basicui.adapter
 
 import android.content.Context
+import android.graphics.Color
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.peakmain.basicui.R
 import com.peakmain.basicui.bean.CategoryRightBean
-import com.peakmain.basicui.bean.CategoryRightSubBean
 import com.peakmain.ui.recyclerview.adapter.CommonRecyclerAdapter
 import com.peakmain.ui.recyclerview.adapter.ViewHolder
 
@@ -20,10 +21,26 @@ class MenuRightRecyclerAdapter(context: Context?, data: List<CategoryRightBean>)
         data,
         R.layout.item_recycler_menu_categroy_right
     ) {
+    private var mSelectPosition: Int = 0
+    private var mOldSelectPosition = mSelectPosition
     override fun convert(holder: ViewHolder, item: CategoryRightBean) {
         holder.setText(R.id.tv_title, item.title)
+        val textView = holder.getView<TextView>(R.id.tv_title)
+        if (holder.bindingAdapterPosition == mSelectPosition) {
+            textView?.setTextColor(Color.parseColor("#6CBD9B"))
+        } else {
+            textView?.setTextColor(Color.parseColor("#272A2B"))
+        }
         val recyclerView = holder.getView<RecyclerView>(R.id.rv_sub_content)
         recyclerView?.adapter = MenuSubRightRecyclerAdapter(mContext, item.categoryRightBeans)
     }
+    fun setSelectItem(position: Int) {
+        mSelectPosition = position
+        if (mSelectPosition != mOldSelectPosition){
+            notifyItemChanged(mOldSelectPosition)
+            notifyItemChanged(mSelectPosition)
+            mOldSelectPosition=position
+        }
 
+    }
 }
