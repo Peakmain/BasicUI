fca81f243c7a51910c968a97a84ecce2afe94991
peakmain <2726449200@qq.com>
2023-12-04 14:09:34 +0800

    添加ViewExt

--- ui/src/main/java/com/peakmain/ui/ext/ViewExt.kt
+++ ui/src/main/java/com/peakmain/ui/ext/ViewExt.kt
@@ -1,7 +1,12 @@
 package com.peakmain.ui.ext
 
 import android.view.View
+import android.view.ViewTreeObserver
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.constraintlayout.widget.Group
+import androidx.core.content.ContextCompat
 
 /**
  * author ：Peakmain
@@ -16,4 +21,44 @@ fun View.addOnGlobalLayoutListener(block: ((Int) -> Unit)? = null) {
             block?.invoke(measuredHeight)
         }
     })
+}
+
+fun View.addOnPreDrawListener(block: ((Int) -> Unit)? = null) {
+    viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+        override fun onPreDraw(): Boolean {
+            viewTreeObserver.removeOnPreDrawListener(this)
+            block?.invoke(measuredHeight)
+            return true
+        }
+
+    })
+}
+
+fun View.getTopLocationOnScreen(): Int {
+    val location = IntArray(2)
+    getLocationOnScreen(location)
+    return location[1]
+}
+
+fun Group.setOnGroupClickListener(block: (() -> Unit)? = null) {
+    val parentView = parent as? View
+    val count = referencedIds.size
+    for (i in 0 until count) {
+        val id: Int = referencedIds[i]
+        val view = parentView?.findViewById<View>(id)
+        view?.setOnClickListener {
+            block?.invoke()
+        }
+    }
+}
+
+fun TextView?.setTextColorCompat(@ColorRes id: Int) {
+    this?.let {
+        setTextColor(ContextCompat.getColor(context, id))
+    }
+}
+fun TextView?.setBackgroundColorCompat(@ColorRes id: Int) {
+    this?.let {
+        setBackgroundColor(ContextCompat.getColor(context, id))
+    }
 }
